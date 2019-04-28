6b850cac297fe255866bc569389900189a14118f
fastalgo <chinayouyang1991@gmail.com>
2019-04-28 21:09:06 +0000

    u

---
+++ lamb/acc.py
@@ -0,0 +1,22 @@
+import sys
+import numpy as np
+
+print 'Usage: python acc.py Filename'
+filename = sys.argv[1];
+config = ""
+best_acc = 100.0
+with open(filename) as f:
+    for line in f:
+        if "learning_rate " in line:
+            print(line)
+        if "warmup" in line:
+            print(line)
+        if "accuracy" in line:
+            print(line)
+            #mystr = line.split();
+            #acclist = mystr[-1].split('%');
+            #acc = float(acclist[0]);
+            #if acc < best_acc:
+                #best_acc = acc
+                #print(config)
+                #print("lowest error rate: " + str(best_acc))
---
+++ lamb/run.sh
@@ -0,0 +1,11 @@
+#export PYTHONPATH="${PYTHONPATH}:/usr/share/models/"
+#python /usr/share/models/official/mnist/mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000
+for i in 5
+do
+	for j in 0.018 0.018 0.018
+	do
+		echo "warmup $i, learning_rate $j"
+		gsutil -m rm -R -f gs://bert-pretrain-data/cifar/cifar10jobs/*
+		python3 david_net.py --warm_up=$i --learning_rate=$j --weight_decay=0.01
+	done
+done
