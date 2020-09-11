57a2b11b3734e895c162a1301b5b98a6d3ea48a7
fastalgo <chinayouyang1991@gmail.com>
2020-09-11 01:25:42 +0000

    u

--- lamb_test/run.sh
+++ lamb_test/run.sh
@@ -1,8 +1,7 @@
 #export PYTHONPATH="${PYTHONPATH}:/usr/share/models/"
-#python /usr/share/models/official/mnist/mnist_tpu.py --tpu=infer --use_tpu=True --data_dir=gs://bert-pretrain-data/mnist/ --model_dir=gs://bert-pretrain-data/mnist_log/ --iterations=500 --train_steps=2000
 for i in 6
 do
-	for j in 0.05 0.04 0.03 0.025 0.02 0.015  0.015 0.01
+	for j in 0.02
 	do
 		echo "warmup $i, learning_rate $j"
 		gsutil -m rm -R -f gs://cifar10-data/cifar10jobs/*
